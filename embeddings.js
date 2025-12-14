e44d8674070e02dc2f1ce0e2c179f7aaa50d2e0b
aslam48 <Aslamiliyas123$>
2025-12-14 10:53:29 +0100

    working on adding database

--- embeddings.js
+++ embeddings.js
@@ -1,8 +1,38 @@
+// import { OpenAIEmbeddings } from '@langchain/openai'
+// import { MemoryVectorStore } from "@langchain/classic/vectorstores/memory";
+// import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
+// import { Document } from "@langchain/core/documents";
+// import { PGVectorStore } from '@langchain/community/vectorstores/pgvector';
+
+// // emembed chunks 
+// const embeddings = new OpenAIEmbeddings({
+//   model:"text-embedding-3-large"
+// });
+
+
+// export const vectorStore = new MemoryVectorStore(embeddings);
+
+// export const addYTVideosToVectorStore = async (videoData) => {
+//     const { transcript, video_id } = videoData;
+// const docs = [new Document({
+//   pageContent: transcript,
+//   metadata: { video_id }
+// })];
+
+// const splitter = new RecursiveCharacterTextSplitter({
+//   chunkSize: 1000,
+//   chunkOverlap: 200,
+// });
+
+// const chunks = await splitter.splitDocuments(docs);
+// await vectorStore.addDocuments(chunks);
+// }
+
+
 import { OpenAIEmbeddings } from '@langchain/openai'
-import { MemoryVectorStore } from "@langchain/classic/vectorstores/memory";
 import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
 import { Document } from "@langchain/core/documents";
-
+import { PGVectorStore } from '@langchain/community/vectorstores/pgvector';
 
 // emembed chunks 
 const embeddings = new OpenAIEmbeddings({
@@ -10,7 +40,19 @@ const embeddings = new OpenAIEmbeddings({
 });
 
 
-export const vectorStore = new MemoryVectorStore(embeddings);
+export const vectorStore = await PGVectorStore.initialize(embeddings,{
+  postgresConnectionOptions: {
+    connectionString: process.env.DB_URL,
+  },
+  tableName: 'transcripts',
+  columns: {
+    idColumnName: 'id',
+    embeddingColumnName: 'vector',
+    contentColumnName: 'content',
+    metadataColumnName: 'metadata',
+  },
+  distanceStrategy: 'cosine',
+});
 
 export const addYTVideosToVectorStore = async (videoData) => {
     const { transcript, video_id } = videoData;
